f9bea24b68ea2262e01061767b47db5075a11002
seangraves <seankgraves@gmail.com>
2020-01-18 20:35:10 -0500

    nav forwarding

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -26,4 +26,23 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert flash.empty?
     assert_redirected_to root_url
   end
+
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    assert_not_nil session[:forwarding_url]
+    log_in_as(@user)
+    assert_redirected_to edit_user_path(@user)
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name:  name,
+                                    email: email,
+                                    password:              "foobar",
+                                    password_confirmation: "foobar" }}
+    assert_not flash.empty?
+    assert_redirected_to @user
+    assert_nil session[:forwarding_url]
+    @user.reload
+    assert_equal @user.name,  name
+    assert_equal @user.email, email
+  end
 end
